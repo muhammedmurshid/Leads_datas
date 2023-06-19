ff42e10803c20daabc2026cd40de4831b355c768
Murshid <muhsink00000@gmail.com>
2023-06-19 19:58:32 +0530

    module name changed voxbay connector

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,13 @@
 {
-    'name': "Chat Api",
+    'name': "Voxbay Connector",
     'version': "14.0.1.0",
     'sequence': "0",
     'depends': ['base'],
     'data': [
 
-
     ],
     'demo': [],
-    'summary': "api_chat",
+    'summary': "api_leads",
     'description': "this_is_my_app",
     'installable': True,
     'auto_install': False,
--- controllers/main.py
+++ controllers/main.py
@@ -2,11 +2,27 @@ from odoo import http
 from odoo.http import request
 
 
-class MyController(http.Controller):
-    @http.route('/my_route', type='http', auth='public', methods=['POST'])
+class LeadsDatas(http.Controller):
+    @http.route('/voxbayconnector', type='http', auth='public', methods=['POST'])
     def handle_post_request(self, **post):
         # Access the POST data
-        field_value = post.get('field_name')
+        called_number = post.get('called_number')
+        caller_number = post.get('caller_number')
+        call_u_uid = post.get('call_u_uid')
+        agent_number = post.get('agent_number')
+        total_call_duration = post.get('total_call_duration')
+        call_date = post.get('call_date')
+        call_status = post.get('call_status')
+        recording_url = post.get('recording_url')
+        call_start_time = post.get('call_start_time')
+        call_end_time = post.get('call_end_time')
+        conversation_duration = post.get('conversation_duration')
+        dtmf = post.get('dtmf')
+        extension = post.get('extension')
+        destination = post.get('destination')
+        date = post.get('date')
+        duration = post.get('duration')
+        transferred_number = post.get('transferred_number')
 
         # Process the data as needed
         # ...
